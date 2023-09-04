2caa84319014bc0abd6d4296dd7854be9da6c1d1
tharinDmahale <deelaka.mahale@gmail.com>
2023-09-04 13:09:46 +0530

    Initial commit

---
+++ docs/precache-manifest.36797029df6c05b127ae948af5243e38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1151e17d7e11ba6f55838b052b4469d",
+    "url": "/Simple-React-Dapp/index.html"
+  },
+  {
+    "revision": "ff537cd7a7c7572485f1",
+    "url": "/Simple-React-Dapp/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "2185fe7b903559fd66c6",
+    "url": "/Simple-React-Dapp/static/js/2.bd94f0d0.chunk.js"
+  },
+  {
+    "revision": "ff537cd7a7c7572485f1",
+    "url": "/Simple-React-Dapp/static/js/main.626aae18.chunk.js"
+  },
+  {
+    "revision": "da9d611dc6af31eddb4f",
+    "url": "/Simple-React-Dapp/static/js/runtime-main.d949a830.js"
+  }
+]);
---
+++ docs/precache-manifest.5a036810ba7529138e606afd0a64805f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "712ad81f33b78ec331187b3609ee004b",
+    "url": "/Simple-React-Dapp/index.html"
+  },
+  {
+    "revision": "281f02fb39f7ea079145",
+    "url": "/Simple-React-Dapp/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "2185fe7b903559fd66c6",
+    "url": "/Simple-React-Dapp/static/js/2.bd94f0d0.chunk.js"
+  },
+  {
+    "revision": "281f02fb39f7ea079145",
+    "url": "/Simple-React-Dapp/static/js/main.0a5fe789.chunk.js"
+  },
+  {
+    "revision": "da9d611dc6af31eddb4f",
+    "url": "/Simple-React-Dapp/static/js/runtime-main.d949a830.js"
+  }
+]);
